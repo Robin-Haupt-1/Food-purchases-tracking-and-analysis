564f58a025f9534bdf5164aea2bedc72873d8917
Robin-Haupt-1 <robinhaupt@outlook.com>
2022-04-02 17:16:14 +0200

    list items with temporary id

--- py-prototyping/cli.py
+++ py-prototyping/cli.py
@@ -81,40 +81,50 @@ class FoodPCLI:
             self.log("Selected " + colored(date.strftime('%Y-%m-%d'), "green"))
 
             while True:
-                self.log('Search for item (input "n" to create a new one, - to update date and store"')
+                self.log('Search for item (input "n" to create a new one, - to update date and store')
                 _input = input("Search: ").strip()
+                selected_item: Union[AbstractProductItem, ConcreteProductItem, None] = None
+                while selected_item is None:
+                    if _input == "n":
+                        # create new item
+                        self.sync()
+                        continue
+                        pass
+                    elif _input == "-":
+                        date = None
+                        selected_store = None
+                        break
 
-                if _input == "n":
-                    # create new item
+                    cost: int = None  # in cents
+                    concrete_item: ConcreteProductItem = None
+                    abstract_Item: AbstractProductItem = None
+
+                    measurement: int = None  # in ml/g
+                    amount: int = None
+
+                    self.log("Abstract items:")
+                    self.log("---------------")
+                    for i in self.abstract_items:
+                        if _input in i.name:
+                            self.log(f'{temp_item_id_helper.get_id(i)}: {i.name} ({i.metric})', color="yellow")
+                    self.log("")
+
+                    self.log("Concrete items:")
+                    self.log("---------------")
+                    for i in self.concrete_items:
+                        if _input in i.name:
+                            a = self.get_abstract_item(i.abstractItemID)
+                            self.log(f'{temp_item_id_helper.get_id(i)}: {i.name} ({i.brand}) {i.measurement} {a.metric}', color="yellow")
+
+                    _input = input("ID or new search phrase: ").strip()
+
+                    try:
+                        id = int(_input)
+
+                    except Exception as e:
+                        pass
+
                     self.sync()
-                    continue
-                    pass
-                elif _input == "-":
-                    date = None
-                    selected_store = None
-                    break
-
-                cost: int = None  # in cents
-                concrete_item: ConcreteProductItem = None
-                abstract_Item: AbstractProductItem = None
-                measurement: int = None  # in ml/g
-                amount: int = None
-
-                self.log("Concrete items:")
-                self.log("---------------")
-                for i in self.concrete_items:
-                    if _input in i.name:
-                        a = self.get_abstract_item(i.abstractItemID)
-                        self.log(f'{i.ID}: {i.name} ({i.brand}) {i.measurement} {a.metric}', color="yellow")
-                self.log("")
-                self.log("Abstract items:")
-                self.log("---------------")
-                for i in self.abstract_items:
-                    if _input in i.name:
-                        self.log(f'{-i.ID}: {i.name} ({i.metric})', color="yellow")
-
-                self.sync()
-                # start entering purchase procedure
 
 
 cli = FoodPCLI()
